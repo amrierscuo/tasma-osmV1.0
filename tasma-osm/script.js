50f36bd45907622059239b5849ad41efadc82d15
amrierscuo <m.aresu27@studenti.unica.it>
2023-07-02 00:10:06 +0200

    Versione V1.1

---
+++ tasma-osm/script.js
@@ -0,0 +1,9 @@
+.additional-popup {
+    background-color: #f9ca24;
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+    max-width: 300px;
+    text-align: center;
+}
